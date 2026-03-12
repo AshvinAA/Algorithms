76ba8c78e4912a1db74c7a3fd43018cec2639656
AshvinAA <ashvin.afroz@gmail.com>
2026-03-12 12:52:53 +0600

    ATaskBDone

--- A03/TaskB.py
+++ A03/TaskB.py
@@ -1,3 +1,21 @@
 #COUNT THE INVERSION REVISITED
-
 import bisect
+
+import sys
+
+
+input,output= sys.stdin.readline , sys.stdout.write
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+matches=[]
+count=0
+
+for i in range(n-1 , -1 , -1):
+    index = bisect.bisect_left(matches, arr[i])
+    count+=index
+    bisect.insort(matches , arr[i]**2)
+
+
+output(str(count))
