5913129b95bf7320ab1b0005baae1501dbb2002b
AshvinAA <ashvin.afroz@gmail.com>
2026-03-13 15:41:46 +0600

    Fast Power Drift Complete (TaskC)

--- A03/TaskC.py
+++ A03/TaskC.py
@@ -1,14 +1,27 @@
-def modder(a,b):
+#FAST POWER DRIFT
+
+import sys
+
+
+input,output= sys.stdin.readline , sys.stdout.write
+
+def fast_power_drift(a,b,m):
     if b==0:
         return 1
     if b==1:
-        return a
+        return a%m
+    
+    half = fast_power_drift(a,b//2,m)
+    squared = (half * half)%m
+    
+    
     if(b%2==1):
-        return a*modder(a,b//2)*modder(a,b//2)
+        return (a*squared)%m
     else:
-        return modder(a,b//2)*modder(a,b//2)
+        return squared
         
         
-x=modder(2,3)
+a,b = map(int, input().split())
 
-print(x)
+output(str(fast_power_drift(a,b,107)))
+
