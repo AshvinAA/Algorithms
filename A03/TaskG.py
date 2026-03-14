c0c71da112ecb7523d74339032aa0213b00dba6e
AshvinAA <ashvin.afroz@gmail.com>
2026-03-14 23:57:31 +0600

    220Trees but flawed

--- A03/TaskG.py
+++ A03/TaskG.py
@@ -2,25 +2,39 @@
 
 import sys
 from collections import deque
-class Node:
-    def __init__(self,rt):
-        rt=self.rt
-        l=self.l
-        r=self.r
+
 
 input,output= sys.stdin.readline , sys.stdout.write
 n = int(input())
 inOrderTree = list(map(int, input().split()))
 preOrderTree = list(map(int, input().split()))
 
-def treeBuilder(preStart,preEnd,inStart,inEnd,inOrderTree,preOrderTree,element):
-    if(inStart == inEnd):
-        return Node(preOrderTree[inStart])
+def get_PostOrder(preOrder , inOrder):
+    if not preOrder or not inOrder:
+        return []
     
-    node=Node(preOrderTree[preStart])
+    root_val=preOrder[0]
     
-    iSL=0
-    iEL=inOrderTree.index(element)-1
-    pSL=preOrderTree.index(element)+1
-    pEL=pSL+iEL+1
-    node.left=treeBuilder(pSL,pEL,iSL,iEL,inOrderTree,preOrderTree)
+    root_idx=preOrder.index(root_val)
+    
+    #splitting InOrder array
+    
+    left_inOrder = inOrder[:root_idx]
+    right_inOrder = inOrder[root_idx+1:]
+    
+    #splitting PostOrder array
+    
+    left_len = len(left_inOrder)
+    
+    left_preOrder = preOrder[1:1+left_len]
+    right_preOrder = preOrder[1+left_len:]
+    
+    #getting right/left PostOrder array
+    left_post = get_PostOrder(left_preOrder,left_inOrder)
+    right_post=get_PostOrder(right_preOrder,right_inOrder)
+    
+    #returning post order = L R root
+    return left_post + right_post + [root_val]
+    
+postOrderTree=get_PostOrder(preOrderTree,inOrderTree)
+print(*postOrderTree)
