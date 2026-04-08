b2d395229634c1085225318d499203a9f75af7b2
AshvinAA <ashvin.afroz@gmail.com>
2026-04-08 21:43:43 +0600

    Task C Aqssignment04

--- A04/TaskC.py
+++ A04/TaskC.py
@@ -1,7 +1,20 @@
 import sys 
 
 input,output= sys.stdin.readline , sys.stdout.write
-N=input().split()
+N=int(input())
 
 #putting all 0s in the array
 matrix = [[0 for _ in range(N)] for _ in range(N)]
+for i in range(N):
+    u=list(map(int, input().split()))
+    
+    if(u[0] == 0 ):
+        continue
+
+    for j in range(1,len(u)):
+        matrix[i][u[j]] = 1
+    
+for row in matrix:
+    output(" ".join(map(str,row))+"\n")
+
+        
